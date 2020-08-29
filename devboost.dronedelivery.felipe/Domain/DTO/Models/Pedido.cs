005f4c514b5323fcad1e887b5dbb1b00d2239e64
Lucas Scheid <lucas.scheid@penske.com>
2020-08-29 12:46:29 -0300

    Movimentando pastas

--- devboost.dronedelivery.felipe/Domain/DTO/Models/Pedido.cs
+++ devboost.dronedelivery.felipe/Domain/DTO/Models/Pedido.cs
@@ -7,11 +7,22 @@ namespace devboost.dronedelivery.felipe.DTO.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Peso deve ser informado!")]
+        [Range(1, int.MaxValue, ErrorMessage = "A Peso minimo deve ser 1.")]
         public int Peso { get; set; }
+
+        [Required(ErrorMessage = "Latitude deve ser informada!")]
         public double Latitude { get; set; }
+
+        [Required(ErrorMessage = "Longitude deve ser informada!")]
         public double Longitude { get; set; }
+        
         public DateTime DataHoraInclusao { get; set; }
+
+        [Required(ErrorMessage = "Situacao deve ser informada!")]
         public int Situacao { get; set; }
+        
         public DateTime DataUltimaAlteracao { get; set; }
         public DateTime DataHoraFinalizacao { get; set; }
     }
